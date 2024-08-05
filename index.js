d7cdcbabab6842c1477897c2dfd7bc5d89062653
VitoriaDelaciPereira <164279805+VitoriaDelaciPereira@users.noreply.github.com>
2024-08-05 23:01:16 +0000

    05/08

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const meuNome = "Vitória";
 const idade = 17;
 const altura = 1.69;
-const massa = 56;
+const massa = "56 kg";
 const cidade = "São José dos Pinhais";
 
 console.log(meuNome);
 console.log(idade);
+console.log(altura);
+console.log(massa);
+console.log(cidade);
 
 // adicione nesse programa, altura, massa e cidade onde mora
